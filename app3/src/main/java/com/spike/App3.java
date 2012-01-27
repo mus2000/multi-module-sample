d85fd72fe55ba973551eb21dfc1d4a16c39cb6af
Javier Rubio <javier.rubio@bjss.com>
2012-01-27 09:53:31 +0000

    app3

---
+++ app3/src/main/java/com/spike/App3.java
@@ -0,0 +1,17 @@
+package com.spike;
+
+public class App3 {
+	
+	private CoreJ core = new CoreJ();
+	
+	public void method2() {
+		core.hashCode();
+	}
+	
+	public void method2B() {
+		System.out.println("method 2 B2");
+		System.out.println("method 2 B2");
+	}
+	
+}
+
---
+++ app3/src/test/java/com/spike/App3Test.java
@@ -0,0 +1,8 @@
+package com.spike;
+
+public class App3Test {
+	
+	App3 app = new App3();
+	
+}
+
